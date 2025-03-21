c117ab86c1ce89039da2f9fe2fddf50bd6f4cd85
JustHuman228 <justhuman228.github@gmail.com>
2025-03-21 17:23:13 +0300

    Reformat `MoreTotems.java`
    Reformat `MoreTotems.java` according to the
    coding style.

--- src/main/java/com/github/justhm228/moretotems/MoreTotems.java
+++ src/main/java/com/github/justhm228/moretotems/MoreTotems.java
@@ -28,14 +28,26 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MoreTotems extends JavaPlugin {
 
-	@Override
-	public void onEnable() {
-		// Plugin startup logic
+	public MoreTotems() {
 
+		super();
 	}
 
-	@Override
+	@Override()
+	public void onLoad() {
+
+		super.onLoad();
+	}
+
+	@Override()
+	public void onEnable() {
+
+		super.onEnable();
+	}
+
+	@Override()
 	public void onDisable() {
-		// Plugin shutdown logic
+
+		super.onDisable();
 	}
 }
