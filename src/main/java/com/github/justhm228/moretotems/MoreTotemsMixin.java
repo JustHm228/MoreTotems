5bfa7d8f293a9fb9b7a9e402cab7fdcfba089110
JustHuman228 <justhuman228.github@gmail.com>
2025-03-22 18:04:11 +0300

    Add ``findTotem()`` method
    Add a ``findTotem()`` utility method to the
    ``MoreTotemsMixin`` class.

--- src/main/java/com/github/justhm228/moretotems/MoreTotemsMixin.java
+++ src/main/java/com/github/justhm228/moretotems/MoreTotemsMixin.java
@@ -24,10 +24,62 @@
 
 package com.github.justhm228.moretotems;
 
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.entity.EntityResurrectEvent;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
 public class MoreTotemsMixin {
 
 	protected MoreTotemsMixin() {
 
 		super();
+	}
+
+	protected final ItemStack findTotem(final EntityResurrectEvent e) {
+
+		final LivingEntity entity = e.getEntity();
+
+		final EquipmentSlot slot = e.getHand();
+
+		ItemStack totem = null; // If a Totem of Undying is not found, `null` will be returned
+
+		// If there is a Totem of Undying, it will be found:
+		if (slot != null) {
+
+			if (entity instanceof HumanEntity player) {
+
+				// Use the `PlayerInventory` if available
+				final PlayerInventory inv = player.getInventory();
+
+				// Find the Totem of Undying:
+				totem = switch (slot) {
+
+					case HAND -> inv.getItemInMainHand();
+					case OFF_HAND -> inv.getItemInOffHand();
+
+					// This should never happen, but if it somehow happens, here is the code for it:
+					case FEET -> inv.getBoots();
+					case LEGS -> inv.getLeggings();
+					case CHEST -> inv.getChestplate();
+					case HEAD -> inv.getHelmet();
+				};
+
+			} else {
+
+				// Otherwise, use `EntityEquipment` from Paper API:
+				final EntityEquipment equipment = entity.getEquipment();
+
+				if (equipment != null) {
+
+					totem = equipment.getItem(slot);
+				}
+			}
+		}
+
+		return totem;
 	}
 }
